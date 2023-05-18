6914570ca27c7785b371fcc90d90573018530528
XCaramellX <ndsgirlz@gmail.com>
2023-05-18 02:26:47 -0400

    created login and register button and started to make register screen

--- app/src/main/java/com/example/promptmeai/MainActivity.java
+++ app/src/main/java/com/example/promptmeai/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.promptmeai;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterScreen.class);
+                startActivity(registerIntent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/promptmeai/RegisterScreen.java
@@ -0,0 +1,16 @@
+package com.example.promptmeai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+
+
+    }
+}
