e76817ee0c84ac611950f0e447deb45fe46d9603
XCaramellX <ndsgirlz@gmail.com>
2023-05-25 00:50:25 -0400

    created firebase register and login

--- app/src/main/java/com/example/promptmeai/LoginScreen.java
+++ app/src/main/java/com/example/promptmeai/LoginScreen.java
@@ -1,20 +1,96 @@
 package com.example.promptmeai;
 
-import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+
 import android.widget.EditText;
+
+
+import com.google.android.material.textfield.TextInputEditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 public class LoginScreen extends AppCompatActivity{
+
+    private TextInputEditText emailLoginTextInput, passwordLoginTextInput;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    private String TAG = "LoginScreen";
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        emailLoginTextInput = findViewById(R.id.emailLoginText);
+        passwordLoginTextInput = findViewById(R.id.passwordLoginText);
 
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() { // make the login button click, validate, and login
+            @Override
+            public void onClick(View view) {
+             String email, password;
+
+             email = emailLoginTextInput.getText().toString();
+             password = passwordLoginTextInput.getText().toString();
+
+             inputValidation();
+
+             existingUser(email, password);
+            }
+        });
+
+    }
+
+    private boolean inputValidation(){
+        if(TextUtils.isEmpty(emailLoginTextInput.getText().toString().trim())){
+            emailLoginTextInput.setError("Please enter your email");
+            return false;
+        }
+        if(TextUtils.isEmpty(passwordLoginTextInput.getText().toString().trim())){
+            passwordLoginTextInput.setError("Please enter your password");
+            return false;
+        }
+        return true;
+    }
+
+    private void existingUser(String email, String password){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginScreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/promptmeai/RegisterScreen.java
+++ app/src/main/java/com/example/promptmeai/RegisterScreen.java
@@ -5,16 +5,33 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+
 import android.widget.EditText;
+
+
 import com.google.android.material.textfield.TextInputEditText;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 public class RegisterScreen extends AppCompatActivity{
 
    private TextInputEditText emailTextInput, usernameTextInput, passwordTextInput, reEnterPasswordTextInput;
 
+   private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+   private String TAG = "RegisterScreen";
    Button register;
 
     @Override
@@ -22,25 +39,32 @@ public class RegisterScreen extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
 
-        register =findViewById(R.id.registerUser);
+        register = findViewById(R.id.registerUser);
 
         emailTextInput = findViewById(R.id.emailTextInput);
         usernameTextInput = findViewById(R.id.usernameTextInput);
         passwordTextInput = findViewById(R.id.passwordTextInput);
         reEnterPasswordTextInput = findViewById(R.id.reEnterPasswordTextInput);
 
+
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email, password;
 
-                if(inputValidation()){
-                    Intent categoryExperience = new Intent(RegisterScreen.this,
-                            CategoryExperience.class);
-                    startActivity(categoryExperience);
+                email = emailTextInput.getText().toString();
+                password = passwordTextInput.getText().toString();
+
+                if(inputValidation()) {
+
+                    newUser(email, password);
 
                 }
+
             }
         });
+
 
 
     }
@@ -67,4 +91,32 @@ public class RegisterScreen extends AppCompatActivity{
         }
         return true;
     }
+
+    private void newUser(String email, String password){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task){
+                    if(task.isSuccessful()) {
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = auth.getCurrentUser();
+                        Intent categoryExperience = new Intent(RegisterScreen.this,
+                                CategoryExperience.class);
+                        startActivity(categoryExperience);
+
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        String errorMessage = task.getException() != null ? task.getException().getMessage()
+                                : "Unknown Error";
+                        Toast.makeText(RegisterScreen.this, "Authentication failed."
+                                        + errorMessage,
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+    }
+
+
 }
